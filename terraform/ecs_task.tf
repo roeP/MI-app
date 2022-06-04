3b409c7dc9bc9b972325ea7a44acdb9949aa7083
Roe.P <roe.pinhas@walkme.com>
2022-06-04 15:42:52 +0300

    fix deploy

--- terraform/ecs_task.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecs_task_definition" "web_app_task" {
-  family                   = "mi_app"
-  container_definitions    = "${data.template_file.web_task.rendered}"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  cpu                      = "256"
-  memory                   = "512"
-}
-
